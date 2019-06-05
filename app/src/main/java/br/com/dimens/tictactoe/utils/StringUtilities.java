96f508db72d599f773f94ca4567e3cb094008e02
Ricardo Chagas <ricardo.chagas2@outlook.com>
2019-06-05 16:00:21 -0300

    Adds the funcionatility to convert an array of numbers to a String

--- app/src/main/java/br/com/dimens/tictactoe/utils/StringUtilities.java
+++ app/src/main/java/br/com/dimens/tictactoe/utils/StringUtilities.java
@@ -9,4 +9,14 @@ public class StringUtilities {
     public static boolean isEmptyOrNull(String value) {
         return value == null || value.equals(EMPTY_STRING);
     }
+
+    public static String stringFromNumbers(int... numbers) {
+        StringBuilder sNumbers = new StringBuilder();
+
+        for (int number: numbers) {
+            sNumbers.append(number);
+        }
+
+        return sNumbers.toString();
+    }
 }
