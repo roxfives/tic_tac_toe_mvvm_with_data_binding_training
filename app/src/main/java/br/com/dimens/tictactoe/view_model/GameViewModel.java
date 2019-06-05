2d9d0ca340c38271010ca0189dcc9e3fbdb6e0a2
Ricardo Chagas <ricardo.chagas2@outlook.com>
2019-06-05 16:01:14 -0300

    Implements the ViewModel of the application

--- app/src/main/java/br/com/dimens/tictactoe/view_model/GameViewModel.java
+++ app/src/main/java/br/com/dimens/tictactoe/view_model/GameViewModel.java
@@ -1,4 +1,38 @@
 package br.com.dimens.tictactoe.view_model;
 
-public class GameViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+
+import android.databinding.ObservableArrayMap;
+
+import br.com.dimens.tictactoe.model.Cell;
+import br.com.dimens.tictactoe.model.Game;
+import br.com.dimens.tictactoe.model.Player;
+
+import static br.com.dimens.tictactoe.utils.StringUtilities.stringFromNumbers;
+
+public class GameViewModel extends ViewModel {
+
+    public ObservableArrayMap<String, String> cells;
+    private Game game;
+
+    public void init(String player1, String player2) {
+        game = new Game(player1, player2);
+        cells = new ObservableArrayMap<>();
+    }
+
+    public void onClickedCellAt(int row, int column) {
+        if (game.getCells()[row][column] == null) {
+            game.getCells()[row][column] = new Cell(game.getCurrentPlayer());
+            cells.put(stringFromNumbers(row, column), game.getCurrentPlayer().getValue());
+            if (game.hasGameEnded())
+                game.reset();
+            else
+                game.switchPlayer();
+        }
+    }
+
+    public LiveData<Player> getWinner() {
+        return game.getWinner();
+    }
 }
